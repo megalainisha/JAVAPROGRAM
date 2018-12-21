f1cd6690cd70a78d0cefb0bfe78748d9f06b48e6
megalainisha <45972844+megalainisha@users.noreply.github.com>
2018-12-21 15:05:58 +0530

    12/21/2018

--- Hotel booking system/src/Admin.java
+++ Hotel booking system/src/Admin.java
@@ -4,10 +4,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Admin extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -33,8 +38,27 @@ public class Admin extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblAdminPage = new JLabel("ADMIN PAGE");
+		lblAdminPage.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblAdminPage.setBounds(151, 24, 96, 16);
+		contentPane.add(lblAdminPage);
+		
+		JLabel lblUserId = new JLabel("USER ID");
+		lblUserId.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblUserId.setBounds(40, 72, 56, 16);
+		contentPane.add(lblUserId);
+		
+		textField = new JTextField();
+		textField.setBounds(131, 70, 178, 22);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNext = new JButton("NEXT");
+		btnNext.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnNext.setBounds(274, 121, 97, 25);
+		contentPane.add(btnNext);
 	}
-
 }
